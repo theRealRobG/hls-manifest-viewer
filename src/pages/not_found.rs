822d15be4617cd678364156c9edfcba1296c8383
Robert Galluccio <robertnw5@gmail.com>
2025-07-08 21:59:15 -0400

    Cleaned up not found page

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -3,5 +3,11 @@ use leptos::prelude::*;
 /// 404 Not Found Page
 #[component]
 pub fn NotFound() -> impl IntoView {
-    view! { <h1>"Uh oh!" <br /> "We couldn't find that page!"</h1> }
+    view! {
+        <h1 class="body-content">"404 Not Found"</h1>
+        <p class="body-content body-text">
+            "This is a simple site; just home (" <code>"/"</code> ") and about ("
+            <code>"/about"</code> ")."
+        </p>
+    }
 }
