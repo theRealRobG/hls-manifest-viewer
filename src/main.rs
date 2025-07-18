5efd6bfe2ba222fc07dca0d838beee289d3a565a
Robert Galluccio <robertnw5@gmail.com>
2025-07-18 00:43:59 -0400

    Added support for logging

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use hls_manifest_viewer::App;
 use leptos::prelude::*;
 
 fn main() {
+    init_log();
     console_error_panic_hook::set_once();
     mount_to_body(App)
 }
+
+#[cfg(feature = "console_log")]
+fn init_log() {
+    _ = console_log::init_with_level(log::Level::Debug);
+}
+#[cfg(not(feature = "console_log"))]
+fn init_log() {}
